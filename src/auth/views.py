afea9121d1b87e568b2baf31f62ee5508a1b79e1
Carlos Lopez <cmlf1598@gmail.com>
2026-08-10 21:20:29 -0600

    Added register form. Django updated to 6.0

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 # Create your views here.
 def login_view(request):
     if request.method == "POST":
@@ -18,5 +22,17 @@ def login_view(request):
     # GET request — just show the empty login form
     return render(request, "auth/login.html", {})
 
-# def register_view(request):
-#     return render(request, "auth/login.html", {})
+def register_view(request):
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST.get("username") or None
+        email = request.POST.get("email") or None
+        password = request.POST.get("password") or None
+        # query for the db of the username and email
+        # username_exists = User.objects.filter(username__iexact=username).exists()
+        # email_exists = User.objects.filter(email__iexact=username).exists()
+        try:
+            User.objects.create_user(username, email=email, password=password)
+        except:
+            pass
+    return render(request, "auth/register.html", {})
