52b1f4307e71f5db970c0208bb445626b598028b
Carlos Lopez <cmlf1598@gmail.com>
2026-08-14 17:44:06 -0600

    Sync User Subscription with the User Group

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,24 +1,68 @@
 from django.db import models
 from django.contrib.auth.models import Group, Permission
+from django.db.models.signals import post_save
+from django.conf import settings
 
+User = settings.AUTH_USER_MODEL # "auth.User"
+
+ALLOW_CUSTOM_GROUPS = True
 
 SUBSCRIPTION_PERMISSIONS = [
-            ("advanced", "Advanced Perm"), #subscriptions.advanced
-            ("pro", "Pro Perm"), #subscriptions.pro
-            ("basic", "Basic Perm"), #subscriptions.basic
-            ("basic_ai", "Basic AI Perm")
-        ]
+    ("advanced", "Advanced Perm"), #subscriptions.advanced
+    ("pro", "Pro Perm"), #subscriptions.pro
+    ("basic", "Basic Perm"), #subscriptions.basic
+    ("basic_ai", "Basic AI Perm")
+]
 
 
 # Create your models here.
 class Subscription(models.Model):
     name = models.CharField(max_length=120)
-    groups = models.ManyToManyField(Group)
+    active = models.BooleanField(default=True) # when syncing 
+    groups = models.ManyToManyField(Group) #one-to-one
     permissions = models.ManyToManyField(Permission,
     limit_choices_to={"content_type__app_label":"subscriptions",
                       "codename__in": [x[0] for x in SUBSCRIPTION_PERMISSIONS]
                       }
     ) # show only a few choices
 
+    def __str__(self):
+        return f"{self.name}" # to show the actual name in the admin menu
+
+
     class Meta:
         permissions = SUBSCRIPTION_PERMISSIONS
+
+
+class UserSubscription(models.Model):
+    # When user is delete, their subscription as well. 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    subscription = models.ForeignKey(Subscription, 
+    on_delete=models.SET_NULL, null=True, blank=True) # user can or cannot have a subscription
+    active = models.BooleanField(default=True)
+
+
+def user_sub_post_save(sender, instance, *args, **kwargs):
+    user_sub_instance = instance
+    user = user_sub_instance.user
+    subscription_obj = user_sub_instance.subscription
+    groups_ids = []
+    if subscription_obj is not None:
+        groups = subscription_obj.groups.all()
+        groups_ids = groups.values_list("id", flat=True) 
+    if not ALLOW_CUSTOM_GROUPS:
+        user.groups.set(groups_ids)
+    else:
+        subs_qs = Subscription.objects.filter(active=True)
+        if subscription_obj is not None: 
+            subs_qs = subs_qs.exclude(id=subscription_obj.id) # active Subscriptions
+        subs_groups = subs_qs.values_list("groups__id", flat=True)
+        subs_groups_set = set(subs_groups)
+        # groups_ids = groups.values_list('id', flat=True) # ids as a list 
+        current_groups = user.groups.all().values_list('id', flat=True)
+        groups_ids_set = set(groups_ids) # if group is a set, then the union of them 
+        current_groups_set = set(current_groups) - subs_groups_set # my current groups 
+        final_group_ids = list(groups_ids_set | current_groups_set)
+        user.groups.set(final_group_ids)
+
+post_save.connect(user_sub_post_save, sender=UserSubscription)
