d66435cd28c3a3cb886953856ff676ed1d0464bd
Carlos Lopez <cmlf1598@gmail.com>
2026-08-19 16:02:34 -0600

    Featured and ordered Pricing

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from auth import views as auth_views
+from subscriptions import views as subscription_views
 from .views import (
     home_view, 
     about_view, 
@@ -28,8 +29,9 @@ from .views import (
 # where url's go 
 urlpatterns = [
     path("", home_view, name='home'), # index page -> root page 
-    path("login/", auth_views.login_view),
-    path("register/", auth_views.register_view),
+    # path("login/", auth_views.login_view),
+    # path("register/", auth_views.register_view),
+    path("pricing/", subscription_views.subscription_price_view, name="pricing"),
     path("about/", about_view),
     path('hello-world/', home_view),
     path('hello-world.html', home_view),
