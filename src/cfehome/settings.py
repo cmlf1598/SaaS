a72b1f7ed87b986630b52797e2d7fc4057ad8b6c
Carlos Lopez <cmlf1598@gmail.com>
2026-08-20 16:45:48 -0600

    Stripe Checkout Session

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -54,6 +54,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
 DEBUG = config("DJANGO_DEBUG", cast=bool)
 
+BASE_URL = config("BASE_URL", default=None)
 
 ALLOWED_HOSTS = [
     ".railway.app"  #https://saas.prod.railway.app to run django in this domain
