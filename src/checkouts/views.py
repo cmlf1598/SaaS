bd396550da95d0b22c75f8451fee0450aeb4a595
Carlos Lopez <cmlf1598@gmail.com>
2026-08-24 12:19:56 -0600

    Stripe Checkout Finalize View in Django (part 2)

--- src/checkouts/views.py
+++ src/checkouts/views.py
@@ -2,10 +2,14 @@ import helpers.billing
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.http import HttpResponseBadRequest
 
 from django.conf import settings 
 
-from subscriptions.models import SubscriptionPrice
+from subscriptions.models import SubscriptionPrice, Subscription, UserSubscription
+
+User = get_user_model()
 
 BASE_URL = settings.BASE_URL
 # Create your views here.
@@ -40,18 +44,33 @@ def checkout_redirect_view(request):
 def checkout_finalize_view(request):
     # it will grab the id value from the link after checkout. 
     session_id = request.GET.get('session_id')
-    checkout_r = helpers.billing.get_checkout_session(session_id, raw=True)
-    sub_stripe_id = checkout_r.subscription
-    sub_r = helpers.billing.get_subscription(sub_stripe_id, raw=True)
-    sub_plan = sub_r.plan
-    sub_plan_price_stripe_id = sub_plan.id
-    price_qs = SubscriptionPrice.objects.filter(stripe_id=sub_plan_price_stripe_id)
-    customer_id = checkout_r.customer
-    print(price_qs)
-    #print(sub_r)
-    context = {
-        "subscription": sub_r,
-        "checkout": checkout_r,
-    }
+    customer_id, plan_id = helpers.billing.get_checkout_customer_plan(session_id)
+    try:
+        sub_obj = Subscription.objects.get(subscriptionprice__stripe_id=plan_id) # related lookup
+    except:
+        sub_obj = None
+    try:
+        user_obj = User.objects.get(customer__stripe_id=customer_id) # related lookup
+    except:
+        user_obj = None
+
+    _user_sub_exists = False
+    try:
+        _user_sub_obj = UserSubscription.objects.get(user=user_obj)
+        _user_sub_exists = True
+    except UserSubscription.DoesNotExist:
+        _user_sub_obj = UserSubscription.objects.create(user=user_obj, subscription=sub_obj)
+    except:
+        _user_sub_obj = None
+    if None in [sub_obj, user_obj, _user_sub_obj]:
+        return HttpResponseBadRequest("There was an error with your account. Please contact us.")
+    if _user_sub_exists:
+        # cancel old sub
+
+        
+        # assign new sub
+        _user_sub_obj.subscription = sub_obj
+        _user_sub_obj.save()
+    context = {}
 
     return render(request, "checkout/success.html", context)
