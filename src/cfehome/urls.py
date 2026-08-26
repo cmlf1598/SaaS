7203485d2e09d6da779d4d7e23e2d4a2550ad19f
Carlos Lopez <cmlf1598@gmail.com>
2026-08-26 15:38:21 -0600

    A better landing page with Flowbite

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from auth import views as auth_views
 from checkouts import views as checkout_views
+from landing import views as landing_views
 from subscriptions import views as subscription_views
 from .views import (
     home_view, 
@@ -29,9 +30,7 @@ from .views import (
 
 # where url's go 
 urlpatterns = [
-    path("", home_view, name='home'), # index page -> root page 
-    # path("login/", auth_views.login_view),
-    # path("register/", auth_views.register_view),
+    path("", landing_views.landing_page_view, name='home'), # index page -> root page 
     path("checkout/sub-price/<int:price_id>/", 
         checkout_views.product_price_redirect_view,
         name='sub-price-checkout'
